cb58fe2f537d06d01eee80aa6581b2eebab9018f
Mark <mrefermat@hotmail.com>
2018-05-25 07:56:46 -0400

    Fixed quandl bug

--- macro_email.py
+++ macro_email.py
@@ -1,7 +1,5 @@
 import quandl
 import seaborn as sns
-import boto3
-from botocore.exceptions import ClientError
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -12,24 +10,20 @@ token='QWe8iSbyAFzRuod2aroM'
 mkts={'SP 500':'CHRIS/CME_SP1',
       'US 10Y':'CHRIS/CME_TY1',
       'Crude Oil':'CHRIS/CME_CL1',
-      'Eurostoxx 50':'CHRIS/EUREX_FESX1'
+      'Eurostoxx 50':'CHRIS/EUREX_FESX1',
+      'Dollar Index':'CHRIS/ICE_DX1'
       }
 
 data_index=pd.DataFrame()
 for m in mkts.keys():
     try:
-        data_index[m]=quandl.get(mkts[m],token=token).Last
+        data_index[m]=quandl.get(mkts[m],authtoken=token).Last
     except:
-        data_index[m]=quandl.get(mkts[m],token=token).Settle
+        data_index[m]=quandl.get(mkts[m],authtoken=token).Settle
 data_pct=data_index.pct_change()
 
 data_pct['2018':].cumsum().ffill().plot(colormap='brg').get_figure().savefig('YTD.png')
 
 e=Email(to='mark.refermat@gmail.com',subject='Macro YTD Email')
-e.add_attachments(['YTD.png'])
+e.add_attachment('YTD.png')
 e.send()
-
-
-
-
-

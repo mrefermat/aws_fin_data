0aab6dcd0723fd74ed81650c5ca0fd66cdc8e725
Mark <mrefermat@hotmail.com>
2018-06-28 15:12:48 -0400

    remove dropna's

--- lab.py
+++ lab.py
@@ -7,9 +7,9 @@ def calc_pairwise_correlation(corr_matrix):
 
 # Calculate timeseries of pairwise correlation using days look back accros as many numbers as needed
 def calc_ts_pairwise_correlation(data_pct,days=250):
-	corrts=pd.ewmcorr(data_pct.dropna(),days,min_periods=days)
+	corrts=pd.ewmcorr(data_pct,days,min_periods=days)
 	s = pd.Series()
-	for i in data_pct.dropna().index:
+	for i in data_pct.index:
 		x=corrts.ix[i]
 		x=x[x.count()!=0].T[x.count()!=0]
 		s[i]=calc_pairwise_correlation(x)

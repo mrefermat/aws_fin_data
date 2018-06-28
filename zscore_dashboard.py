eacfdfe3b2d35dfeffd8a21aeb5cf60e14311dff
Mark <mrefermat@hotmail.com>
2018-06-28 15:28:48 -0400

    additional markets

--- zscore_dashboard.py
+++ zscore_dashboard.py
@@ -18,14 +18,14 @@ mkts={'SP 500':'CHRIS/CME_SP1',
       'US High Yield':'COM/CDXNAHY',
       'US 20 Year Treasury':'FRED/DGS20',
       'TED Spread':'FRED/TEDRATE',
-      #'EUR':'CURRFX/USDEUR',
-      #'JPY':'CURRFX/USDJPY',
-      #'GBP':'CURRFX/GBPUSD',
-     # 'CHF':'CURRFX/USDCHF',
-     # 'BRL':'CURRFX/USDBRL',
-     # 'CNY':'CURRFX/CNYUSD',
-     # 'RUB':'CURRFX/RUBUSD',
-     # 'TRY':'CURRFX/TRYUSD'
+      'Wheat':'CHRIS/CME_W7',
+      'Corn':'CHRIS/CME_C1',
+      'Dax':'CHRIS/EUREX_FDAX1',
+      'FTSE100':'CHRIS/LIFFE_Z1',
+      'Eurodollar':'CHRIS/CME_ED1',
+      'Euro':'CHRIS/CME_EC1',
+      'GBP':'CHRIS/CME_BP1',
+      'Gold':'CHRIS/CME_EC1'
       }
 
 data_index=pd.DataFrame()
@@ -48,8 +48,8 @@ for m in mkts.keys():
                 		print(m)
 data_pct=data_index.pct_change()  
 
-mu=pd.ewma(data_pct,60)
-sd=pd.ewmstd(data_pct,60)
+mu=pd.ewma(data_pct,120)
+sd=pd.ewmstd(data_pct,120)
 zscores=(data_pct-mu)/sd
 last=zscores.iloc[-2].dropna().sort_values()
 last.plot(kind='barh',colormap='jet',ylim=[-3,3]).get_figure().savefig('zscore.png',bbox_inches='tight' )

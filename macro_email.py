40198e713c1cd8c6cdb9935c3fcf75e51739de63
Mark <mrefermat@hotmail.com>
2018-08-14 18:10:56 -0400

    corrected bug

--- macro_email.py
+++ macro_email.py
@@ -14,8 +14,8 @@ mkts={'SP 500':'CHRIS/CME_SP1',
       'Dollar Index':'CHRIS/ICE_DX1',
       #'US 3M T-bills':'FRED/DTB3',
       #'Fed Funds Effective Rate':'FRED/DFF',
-      'US Investment Grade':'COM/CDXNAIG',
-      'US High Yield':'COM/CDXNAHY',
+      #'US Investment Grade':'COM/CDXNAIG',
+      #'US High Yield':'COM/CDXNAHY',
       #'US 20 Year Treasury':'FRED/DGS20',
       #'TED Spread':'FRED/TEDRATE',
       'Wheat':'CHRIS/CME_W7',
@@ -30,10 +30,22 @@ mkts={'SP 500':'CHRIS/CME_SP1',
 
 data_index=pd.DataFrame()
 for m in mkts.keys():
-    try:
-        data_index[m]=quandl.get(mkts[m],authtoken=token).Last
-    except:
-        data_index[m]=quandl.get(mkts[m],authtoken=token).Settle
+	try:
+		data_index[m]=quandl.get(mkts[m],authtoken=token).Last
+	except:
+		try:
+			data_index[m]=quandl.get(mkts[m],authtoken=token).Settle
+		except:
+			try:
+				data_index[m]=quandl.get(mkts[m],authtoken=token).Value
+			except:
+				try:
+					data_index[m]=quandl.get(mkts[m],authtoken=token).value
+				except:
+					try:
+						data_index[m]=quandl.get(mkts[m],authtoken=token).Rate
+					except:
+						print(m)
 data_pct=data_index.pct_change()
 
 data_pct['2018':].cumsum().ffill().plot(colormap='brg').get_figure().savefig('YTD.png')

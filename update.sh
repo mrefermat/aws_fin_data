fb88ee9256ebfcfad6d8b73691800456400ab6d4
Mark <mrefermat@hotmail.com>
2018-09-07 12:12:50 -0400

    updated crontab

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 /home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/socgen_indices.py &
 /home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/zscore_dashboard.py &
 /home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/pairwise_corr_email.py &
+/home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/sector_data.py &

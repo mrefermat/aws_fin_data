2b5e269365f0e0b9533d12f0b8f4cead6c0eccc3
Mark <mrefermat@hotmail.com>
2018-10-04 07:34:05 -0400

    New factor analysis

---
+++ factor.py
@@ -0,0 +1,36 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+from mail import Email
+import time
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.sectorperformance import SectorPerformances
+key ='EXATBX3CPYNC2QAM'
+sns.set_context("poster")
+sns.set(font_scale=3)
+
+def get_stock_adj_price(ticker):
+	ts = TimeSeries(key=key, output_format='pandas')
+	data, meta_data = ts.get_daily_adjusted(ticker,outputsize='full')
+	data.index = pd.to_datetime(data.index)
+	return data['5. adjusted close']
+
+df=pd.DataFrame()
+df['Min Vol ETF']=get_stock_adj_price('USMV')
+df["Momentum"]=get_stock_adj_price('MTUM')
+df['Quality']=get_stock_adj_price('QUAL')
+df['Value']=get_stock_adj_price('FNDX')
+time.sleep(61)
+df['Size']=get_stock_adj_price('SIZE')
+
+sp=get_stock_adj_price('VOO')
+
+rets=df.dropna().pct_change()
+hedged_factor=rets.subtract(sp.pct_change(),axis=0)
+
+hedged_factor.dropna()['2017':].cumsum().plot(title='Hedge US factor ETF Performance)').get_figure().savefig('factor.png',bbox_inches='tight')
+
+e=Email(to='mark.refermat@gmail.com',subject='Morning Update: Factor Performance')
+e.add_attachment('factor.png')
+e.send()
--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@
 /home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/zscore_dashboard.py &
 /home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/pairwise_corr_email.py &
 /home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/sector_data.py &
+/home/ec2-user/anaconda3/bin/python /home/ec2-user/aws_fin_data/factor.py &

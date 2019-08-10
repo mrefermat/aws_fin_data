5d096f4e2f8326b4725ad8cd96c0c87e243c0ff6
Mark <mrefermat@hotmail.com>
2019-08-10 01:12:13 -0400

    formating issues

--- weekly_email.py
+++ weekly_email.py
@@ -1,16 +1,14 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from mail import Email
 
 quote_page='http://archive.wortfm.org/'
 page = urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
 
-from mail import Email
-
 msg=''
 for l in soup.find_all(sho='83'): 
     msg=msg+ '<a href="' +  l.find('button').get('mp3') +  '">Diaspora</a>\n'
-
 
 e=Email(subject='Weekly Update: Music Email')
 e.add_text(msg)

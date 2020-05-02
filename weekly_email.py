86fb1b73036b5545c26e533dfb66c8c911d4ebc0
Mark <mark@Alices-MacBook-Pro.local>
2020-05-02 11:08:26 -0700

    Fixed bug with show link

--- weekly_email.py
+++ weekly_email.py
@@ -10,7 +10,7 @@ msg=''
 for l in soup.find_all(sho='83'): 
     msg=msg+ '<p><a href="' +  l.find('button').get('mp3') +  '">Diaspora</a>'
 
-for l in soup.find_all(sho='53'): 
+for l in soup.find_all(sho='72'): 
     msg=msg+ '<p><a href="' +  l.find('button').get('mp3') +  '">Global Revolution</a>'
 
 for l in soup.find_all(sho='21'): 
